9a936b6d06eaffd5df4d5591251c6ff919df9360
Valentin Bashkatov <bashkatovvala@gmail.com>
2024-06-19 15:10:27 +0400

    Save the work

--- acme_project/birthday/forms.py
+++ acme_project/birthday/forms.py
@@ -1,18 +1,12 @@
 from django import forms
 
+from .models import Birthday
 
-class BirthdayForm(forms.Form):
-    first_name = forms.CharField(
-        max_length=20,
-        label='Имя'
-    )
-    last_name = forms.CharField(
-        max_length=20,
-        required=False,
-        label='Фамилия',
-        help_text='Необязательное поле'
-    )
-    birthday = forms.DateField(
-        label='Дата рождения',
-        widget=forms.DateInput(attrs={'type': 'date'})
-    )
+
+class BirthdayForm(forms.ModelForm):
+    class Meta:
+        model = Birthday
+        fields = '__all__'
+        widgets = {
+            'birthday': forms.DateInput(attrs={'type': 'date'}),
+        }
--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,1 +1,21 @@
 from django.db import models
+from django.core.validators import (
+    MaxValueValidator,
+    MinValueValidator
+)
+
+
+class Birthday(models.Model):
+    first_name = models.CharField(
+        verbose_name='Имя',
+        max_length=20
+    )
+    last_mane = models.CharField(
+        verbose_name='Фамилия',
+        max_length=20,
+        blank=True,
+        help_text='Необязательное поле'
+    )
+    birthday = models.DateField(
+        verbose_name='Дата рождения'
+    )
